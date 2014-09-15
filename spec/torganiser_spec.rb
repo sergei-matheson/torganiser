8e9668c67ddd5585ca476de58203075943ad088a
Sergei Matheson <sergei.matheson@gmail.com>
2014-09-15 14:51:42 +1000

    Don't use should in rspec 3

--- spec/torganiser_spec.rb
+++ spec/torganiser_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Torganiser do
 
   it 'should have a version number' do
-    Torganiser::VERSION.should_not be_nil
+    expect(Torganiser::VERSION).not_to be_nil
   end
 
 end
